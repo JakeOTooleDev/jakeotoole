02333bae14311066fbb7da3e21d480ae6801cb87
Jake O'Toole <jakeotooledev@gmail.com>
2021-07-09 18:13:52 -0500

    Created basic header with navigation. Added to App. Tested.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+import Header from "./components/layout/Header";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route path="/about">
             <About />
